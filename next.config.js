0413736d83cb715fa6f708b03efb4b54bfab91f2
Ashutosh Kumar <ashutosh@sparq.ai>
2025-02-13 17:55:56 +0530

    add

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['play-lh.googleusercontent.com'], // Add this domain
+  },
+}
+
+module.exports = nextConfig
---
+++ src/app/sitemap.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from 'next'
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `https://qiwecksolutions.com`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    }
+  ]
+}
